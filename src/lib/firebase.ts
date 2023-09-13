dcefe6d157a62c05837022190a1434a6380384f2
unknown <acv2fidel@gmail.com>
2023-09-12 21:49:51 -0400

    project-added

---
+++ src/lib/firebase.ts
@@ -0,0 +1,93 @@
+import { initializeApp } from "firebase/app";
+import "firebase/auth";
+import { doc, getFirestore, onSnapshot } from "firebase/firestore";
+import { getAuth, onAuthStateChanged, type User } from "firebase/auth";
+import { getStorage } from "firebase/storage";
+import { writable, type Readable, derived } from "svelte/store";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyADLpn8mLmoBE2Y0s38CuE0FQ2EIHebNLo",
+  authDomain: "sveltekit-b91cd.firebaseapp.com",
+  projectId: "sveltekit-b91cd",
+  storageBucket: "sveltekit-b91cd.appspot.com",
+  messagingSenderId: "494832793042",
+  appId: "1:494832793042:web:8207ed3f6c7dc41134040b",
+  measurementId: "G-0XLGHSQ658",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore();
+export const auth = getAuth();
+export const storage = getStorage();
+
+/**
+ * @returns a store with the current firebase user
+ */
+function userStore() {
+  let unsubscribe: () => void;
+
+  if (!auth || !globalThis.window) {
+    console.warn('Auth is not initialized or not in browser');
+    const { subscribe } = writable<User | null>(null);
+    return {
+      subscribe,
+    }
+  }
+
+  const { subscribe } = writable(auth?.currentUser ?? null, (set) => {
+    unsubscribe = onAuthStateChanged(auth, (user) => {
+      set(user);
+    });
+
+    return () => unsubscribe();
+  });
+
+  return {
+    subscribe,
+  };
+}
+
+export const user = userStore();
+
+
+/**
+ * @param  {string} path document path or reference
+ * @returns a store with realtime updates on document data
+ */
+export function docStore<T>(
+  path: string,
+) {
+  let unsubscribe: () => void;
+
+  const docRef = doc(db, path);
+
+  const { subscribe } = writable<T | null>(null, (set) => {
+    unsubscribe = onSnapshot(docRef, (snapshot) => {
+      set((snapshot.data() as T) ?? null);
+    });
+
+    return () => unsubscribe();
+  });
+
+  return {
+    subscribe,
+    ref: docRef,
+    id: docRef.id,
+  };
+}
+
+interface UserData {
+  username: string;
+  bio: string;
+  photoURL: string;
+  links: any[];
+}
+
+export const userData: Readable<UserData | null> = derived(user, ($user, set) => { 
+  if ($user) {
+    return docStore<UserData>(`users/${$user.uid}`).subscribe(set);
+  } else {
+    set(null); 
+  }
+});  
